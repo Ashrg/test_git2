e2ed062d1960382851ee07970e89ee99f23553b1
chenrg <zc90696@gmail.com>
2021-07-21 21:48:13 +0800

    First Commit

---
+++ gitdemo/gitdemo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gitdemo
+//
+//  Created by rg chen on 2021/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
